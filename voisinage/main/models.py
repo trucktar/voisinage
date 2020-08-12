f6f20f10b3bd9f89620e274f4ee634cff2eee2e3
trucktar <57882685+trucktar@users.noreply.github.com>
2020-08-12 05:26:13 +0300

    Create required models and their relationships

--- voisinage/main/models.py
+++ voisinage/main/models.py
@@ -1,3 +1,71 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+class Neighbourhood(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    location = models.CharField(max_length=100, unique=True)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Member(models.Model):
+    avatar = models.ImageField(upload_to='avatars/', default='')
+    pseudo = models.CharField(max_length=50)
+    about = models.TextField(max_length=200)
+    location = models.CharField(max_length=100)
+
+    user = models.OneToOneField(settings.AUTH_USER_MODEL,
+                                on_delete=models.CASCADE)
+    hood = models.ForeignKey(Neighbourhood,
+                             on_delete=models.CASCADE,
+                             null=True)
+
+    def __str__(self):
+        return self.pseudo
+
+
+class Business(models.Model):
+    class Meta:
+        verbose_name_plural = 'businesses'
+
+    name = models.CharField(max_length=50, unique=True)
+    website = models.URLField(blank=True)
+    phone = models.CharField(max_length=15)
+    email = models.EmailField()
+
+    hood = models.ForeignKey(Neighbourhood, on_delete=models.CASCADE)
+
+
+class EmergencyService(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    department = models.CharField(max_length=2,
+                                  choices=(('FD', 'Fire Department'),
+                                           ('PS', 'Police Station'),
+                                           ('HS', 'Hospital')))
+    phone1 = models.CharField(max_length=15)
+    phone2 = models.CharField(max_length=15)
+    email = models.EmailField()
+
+    hood = models.ForeignKey(Neighbourhood, on_delete=models.CASCADE)
+
+
+class Post(models.Model):
+    image = models.ImageField(upload_to='uploads/', default='', blank=True)
+    title = models.TextField(max_length=300)
+    body = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    hood = models.ForeignKey(Neighbourhood, on_delete=models.CASCADE)
+    member = models.ForeignKey(Member, on_delete=models.CASCADE)
+
+
+class Comment(models.Model):
+    body = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    member = models.ForeignKey(Member, on_delete=models.CASCADE)
