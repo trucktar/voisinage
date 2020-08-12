b2b70a512a3b7b8e2c5a7495e54dc19eb029bb51
trucktar <57882685+trucktar@users.noreply.github.com>
2020-08-12 05:27:59 +0300

    Setup member autocreation at user registration

--- voisinage/settings.py
+++ voisinage/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'whitenoise.runserver_nostatic',
     'django.contrib.staticfiles',
-    'voisinage.main',
+    'voisinage.main.MainConfig',
 ]
 
 MIDDLEWARE = [
